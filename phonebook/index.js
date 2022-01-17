7710b722dc41bb2f37da27f628b0110e23b9a602
Jayshree Rathi <jayshreerathi111@gmail.com>
2022-01-18 01:59:21 +0200

    Part 3.1-3.4

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,47 +1,58 @@
 const express = require('express')
 const app = express()
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true
-  }
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
 ]
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello WorldS</h1>')
-  })
   
-  app.get('/api/notes', (request, response) => {
-    response.json(notes)
+  app.get('/api/info', (request, response) => {
+    response.send('<h1>Phone book has people</h1>')
   })
 
-  app.get('/api/notes/:id', (request, response) => {
+  app.get('/api/persons', (request, response) => {
+    response.json(persons)
+  })
+  
+
+  app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
+    const person = persons.find(person => persons.id === id)
     
-    if (note) {
-      response.json(note)
+    if (person) {
+      response.json(person)
     } else {
       response.status(404).end()
     }
+  })
+
+  app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(person => person.id !== id)
+    response.status(204).end()
   })
   
   const PORT = 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
+
