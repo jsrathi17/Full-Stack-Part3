5d248b6db53134b7a0f99870ff6bac8e86558748
Jayshree Rathi <jayshreerathi111@gmail.com>
2022-01-28 22:09:34 +0200

    adding upto 3.18

--- phonebook/index.js
+++ phonebook/index.js
@@ -31,7 +31,18 @@ let PERSON = [
     }
 ]
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+app.use(errorHandler)
 
 const generateId = () => {
   const maxId = Math.floor(Math.random()* (10000 - 4) + 4)
@@ -52,19 +63,30 @@ app.get('/', (req, res) => {
     })
   
 
-  app.get('/api/persons/:id', (request, response) => {
+  app.get('/api/persons/:id', (request, response, next) => {
     persons.findById(request.params.id).then(person => {
       if(person){
         response.json(person)
       }else{
         response.status(404).end()
       }
-    })
+    }).catch(error => next(error))
   })
 
- 
+  app.put('/api/persons/:id',  (request, response, next) => {
+    const body = request.body
+  
+    const person = {
+      name: body.name,
+      number: body.number,
+    }
+    persons.findByIdAndUpdate(request.params.id, person, { new: true })
+      .then(updatedPerson => {
+        response.json(updatedPerson)
+      }).catch(error => next(error))
+  })
 
-  app.delete('/api/persons/:id', (request, response) => {
+  app.delete('/api/persons/:id', (request, response, next) => {
     persons.findById(request.params.id)
     .then(person => {
       person.remove()
@@ -72,7 +94,7 @@ app.get('/', (req, res) => {
           console.log('Deleted: ' + deletedPerson)
           response.status(204).end()
         })
-  })
+  }).catch(error => next(error))
   })
   
 
